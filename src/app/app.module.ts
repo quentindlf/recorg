7bb22d59929ef261b04d86d90d92cb087ad74cf2
Quentin de la Faye <quentin.delafaye@ineat.ca>
2020-04-19 22:15:35 -0400

    Added Home in routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { MaterialDesignModule } from './material-design/material-design.module';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MaterialDesignModule } from './material-design/material-design.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
-    MaterialDesignModule
+    MaterialDesignModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
